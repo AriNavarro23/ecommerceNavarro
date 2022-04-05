91ab950e86f0ab72d5eef3d44b05b6d2b86f8b28
AriNavarro23 <94860053+AriNavarro23@users.noreply.github.com>
2022-04-04 23:58:14 -0300

    counter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount'
 
 const App = () => {
   return (
     <div className="App">
         <NavBar/>
         <ItemListContainer greeting='Hola Mundo' />
+        <ItemCount/>
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,22 @@
+const ItemCount = () => {
+    
+    let count = 0
+    
+    const decrement = () => {
+        count = count -1
+    }
+
+    const increment = () => {
+        count = count +1
+    }
+
+    return (
+        <div>
+            <button onClick={decrement}>-</button>
+            <p>{count}</p>
+            <button onClick={increment}>+</button>
+        </div>
+    )
+}
+
+export default ItemCount
